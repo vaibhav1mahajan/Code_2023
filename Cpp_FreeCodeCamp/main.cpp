9cb8ecff1c1c2ada500f469d8265eb50bcbd243c
vaibhav1mahajan <vaibhavmahajan2257@gmail.com>
2023-07-19 19:28:04 +0530

    added javascript folder and some changes in Cpp

--- Cpp_FreeCodeCamp/main.cpp
+++ Cpp_FreeCodeCamp/main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
-using namespace std;
+#include<string>
+// using namespace std;
 
 int main(){
-    cout<<100;
+    int a {20};
+    int age;
+    std::string name;
+    std::cout<<a<< std::endl;
+    std::cout<<"Please enter your name and age"<<std::endl;
+    // std::clog<<"Some error might have in this program";
+    // std::cin>>name>>age;
+    std::getline(std::cin,name);
+    std::cin>>age;
+    std::cout<< name<<std::endl;
+    std::cout<<age<<std::endl;
+    return 0;
 } 
---
+++ Jacascript/index.js
@@ -0,0 +1,4 @@
+let a = "1";
+let b = Number(a);
+console.log(b);
+console.log(typeof a , typeof b);
