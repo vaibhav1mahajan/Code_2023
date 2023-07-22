0377426b07e5e9b8b4465b6809f68a52970068ed
vaibhav1mahajan <vaibhavmahajan2257@gmail.com>
2023-07-22 20:02:58 +0530

    few lines code added

--- DSA/Recurssion/allProgram.java
+++ DSA/Recurssion/allProgram.java
@@ -4,11 +4,18 @@ public class allProgram {
     public static void main(String[] args) {
         System.out.println(sum(5));
         
+        
     }
     public static int sum(int n){
         if(n==1){
             return 1;
         }
         return n + sum(n-1);
+    }
+    public static boolean isPalin(String s ,int index){
+        if(index>=s.length()/2){
+            return true;
+        }
+        return s.charAt(index)==s.charAt(s.length()-index-1) && isPalin(s,index+1); 
     }
 }
