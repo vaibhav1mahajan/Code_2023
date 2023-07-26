60aa66cd44dfae69b84abcc323bbc7207265230b
vaibhav1mahajan <vaibhavmahajan2257@gmail.com>
2023-07-26 18:28:26 +0530

    solution of question related to LCS added

--- DSA/DP/variationOfLCS.java
+++ DSA/DP/variationOfLCS.java
@@ -1,33 +1,12 @@
 package DSA.DP;
+
 import java.util.Arrays;
 
-public class mainLCS {
+public class variationOfLCS {
     public static void main(String[] args) {
-        String str = "abcdf";
-        String s = "abcdaf";
-        System.out.println(PrintLCS(str, s));
-    }
-    public static int bottomUp(String str, String s){
-        int n = str.length();
-        int m = s.length();
-        int[][]  dp = new int[n+1][m+1];
-        Arrays.fill(dp[0],0);
-        for(int i =0;i<n+1;i++){
-            dp[i][0] =0;
-        }
-        for(int i =1;i<n+1;i++){
-            for(int j =1;j<m+1;j++){
-                if(str.charAt(i-1)==s.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
         
     }
-    public static String PrintLCS(String str,String s){
+    public static int[][] table(String str, String s){
         int n = str.length();
         int m = s.length();
         int[][]  dp = new int[n+1][m+1];
@@ -44,23 +23,44 @@ public class mainLCS {
                 }
             }
         }
-        StringBuilder ans = new StringBuilder();
-        int i =n;
-        int j =m;
-        while(i>0 && j>0){
-            if(str.charAt(i-1)==s.charAt(j-1)){
-                ans.append(str.charAt(i-1));
-                i--;
-                j--;
-            } else{
-                if(dp[i-1][j]>dp[i][j-1]){
-                    i--;
+        return dp;
+    }
+    public static int LongestCommonSubstring(String str, String s){
+        int n = str.length();
+        int m = s.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i =0;i<n+1;i++){
+            dp[i][0] =0;
+        }
+        for(int j =0;j<m+1;j++){
+            dp[0][j] =0;
+        }
+        int max1 = -1;
+        for(int i =1;i<n+1;i++){
+            for(int j =1;j<m+1;j++){
+                if(str.charAt(i-1)==s.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 } else{
-                    j--;
+                    dp[i][j] =0;
                 }
+                max1 = Math.max(max1,dp[i][j]);
             }
         }
-        return ans.reverse().toString();
+        return max1;
+    }
+
+    public static int ShortestCommonSuperString(String str,String s){
+        int n = str.length();
+        int m = s.length();
+        int[][] dp = table(str, s);
+        return n+m-dp[n][m];
+    }
+    public static int MinNumberOfDeletionAndInsertion(String str , String s){
+        int n = str.length();
+        int m = s.length();
+        int[][] dp = table(str, s);
+        int deletion = n-dp[n][m];
+        int insertion = m-dp[n][m];
+        return insertion + deletion;
     }
 }
-
