88522d587fbfc034761ec120ef9c90991e675143
vaibhav1mahajan <vaibhavmahajan2257@gmail.com>
2023-12-01 13:31:23 +0530

    after a long time

---
+++ DSA/Trees/Course-1/Binary_Trees/BinaryTree.java
@@ -0,0 +1,18 @@
+
+public class BinaryTree {
+
+    private static class Node(){
+
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value){
+            this.value=value;
+        }
+    }
+
+    private Node root;
+    
+    
+}
